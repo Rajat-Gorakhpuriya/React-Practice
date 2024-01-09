5bf720fedd4b5458bd97366e0caae3ad0bbac8d7
Rajat Gorakhpuriya <rajat.gorakhpuriya@gmail.com>
2024-01-09 22:39:15 +0530

    react_practice directory added

---
+++ README.md
@@ -0,0 +1,1 @@
+- It is a React Practice branch
---
+++ app.js
@@ -0,0 +1,16 @@
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        [
+            React.createElement("h1",{},"I am a child"),
+            React.createElement("h2",{},"Heading2 sibling")
+        ]
+    )
+    );
+// const heading = React.createElement("h1", {id: 'heading'}, "Hello Rajat");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+root.render(parent);
